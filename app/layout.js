0052126a915f2c68075c830fb1337f3ade9309eb
ericnstevenstx-lang <ericnstevenstx@gmail.com>
2026-04-13 14:23:49 -0500

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,33 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Hardin eBay Lister',
+  description: 'Comp pricing, SEO listings, nameplate scanning - Hardin Electrical Group',
+  manifest: '/manifest.json',
+  themeColor: '#58815a',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+    viewportFit: 'cover',
+  },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black-translucent',
+    title: 'Hardin Lister',
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,7 @@
+'use client';
+
+import HardinLister from '../components/HardinLister';
+
+export default function Home() {
+  return <HardinLister />;
+}
